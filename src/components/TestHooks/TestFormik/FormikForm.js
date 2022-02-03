7619bc86e8a8d551941e1e8a10d703dcc7539228
NachoPedrosa <ignaciopedrosa1998@gmail.com>
2022-02-03 17:48:31 -0300

    Validando con formik

--- src/components/TestHooks/TestFormik/FormikForm.js
+++ src/components/TestHooks/TestFormik/FormikForm.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { Formik } from 'formik'
+import { Form } from 'react-bootstrap'
+import FormatInput from '../../FormatInput'
+import * as yup from 'yup'
+const validateContactForm = yup.object().shape({
+  name: yup.string().required().max(10),
+  email: yup.string().email(),
+})
 
 const FormikForm = () => {
   return (
     <>
-      <Formik initialValues={{ email: '' }} validationSchema={}></Formik>
+      <Formik
+        initialValues={{ email: '' }}
+        validationSchema={validateContactForm}
+      >
+        <Form>
+          <FormatInput />
+          <FormatInput />
+        </Form>
+      </Formik>
     </>
   )
 }
--- src/components/TestHooks/TestHooks.js
+++ src/components/TestHooks/TestHooks.js
@@ -3,6 +3,7 @@ import NavBar from '../Navbar/NavBar'
 import CounterApp from './CounterApp/CounterApp'
 import { Container } from 'react-bootstrap'
 import InputSearch from './InputSearch/InputSearch'
+import FormikForm from './TestFormik/FormikForm'
 const TestHooks = () => {
   return (
     <div className="cont">
@@ -11,6 +12,7 @@ const TestHooks = () => {
         <Container className="d-flex">
           <CounterApp />
           <InputSearch />
+          <FormikForm />
         </Container>
       </div>
     </div>
